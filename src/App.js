11ffe06d72f5ac94a850c63ba929a7f6a5489b56
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2023-09-11 11:52:58 -0500

    basic framework made ready for cookies

--- src/App.js
+++ src/App.js
@@ -4,11 +4,37 @@ import Cookies from 'universal-cookie';
 import React, { useState } from 'react';
 
 function App() {
-  return (
-      <div class="container-fluid bg-dark p-3 min-vh-100">
-          <h2>Cookie Counter</h2>
-      </div>
-  );
+    const [count, setCount] = useState(0);
+
+    let incrementCount = () => {
+        setCount(count + 1);
+    };
+
+    let decrementCount = () => {
+        if (count < 5) return false;
+        setCount(count - 5);
+    };
+
+    let resetCount = () => {
+        setCount(0);
+    }
+
+    return (
+        <div class="container-fluid bg-dark p-3 min-vh-100">
+            <div class="row text-light text-center">
+                <h2>Cookie Counter</h2>
+                <div class="col-sm-6">
+                    <button onClick={incrementCount}>cookie</button>
+                    <h5>Cookies: {count}</h5>
+                    <button onClick={resetCount}>reset</button>
+                </div>
+                <div class="col-sm-6">
+                    <button onClick={decrementCount}>mouse</button>
+                    <h5>the mouse takes 5 cookies</h5>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
